e06c2225c02e6c2f685b331780db9078fc328e1a
David Ayala and Sara Jensen <sara@>
2016-10-10 16:45:07 -0700

    add notes and update build files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
+// gulp is a package manager that optimizes code and serves it to the browser
 var gulp = require('gulp');
+// browserify compiles code so the browser can understand it
 var browserify = require('browserify');
+// vinyl-source-stream puts browserify source code into a new file
 var source = require('vinyl-source-stream');
+// concat consolidates all js files into one
 var concat = require('gulp-concat');
+// uglify minifies scripts, e.g. removes all unncessary characters while preserving its functionality
 var uglify = require('gulp-uglify');
+// gulp-util allows us to build production or dev environments (using $gulp build or $gulp build --production)
 var utilities = require('gulp-util');
+// buildProduction is a sister to gulp-util that lets us build a prod environment
 var buildProduction = utilities.env.production;
+// with every build, we wipe the slate clean to make sure we're using the latest files by cleaning up with del.
 var del = require('del');
+// a linter that analyzes code and warns us about stuff that doesn't follow code conventions
 var jshint = require('gulp-jshint');
 
 gulp.task('myTask', function(){
