e16656b812c9d8b17669f6f5f4b5941aaa966036
Sara Jay <jensese@MacBook-Pro.domain>
2016-10-10 21:49:24 -0700

    add front end dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -16,13 +16,29 @@ var buildProduction = utilities.env.production;
 var del = require('del');
 // a linter that analyzes code and warns us about stuff that doesn't follow code conventions
 var jshint = require('gulp-jshint');
+// ?
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
+// implement our development server with live reloading
+var browserSync = require('browser-sync').create();
+
+
 
 gulp.task('myTask', function(){
   console.log('hello gulp');
 });
 
 gulp.task('concatInterface', function() {
-  return gulp.src(['../js/*-interface.js'])
+  return gulp.src(['./js/*-interface.js'])
     .pipe(concat('allConcat.js'))
     .pipe(gulp.dest('./tmp'));
 });
@@ -58,10 +74,40 @@ gulp.task("build", ['clean'], function(){
   } else {
     gulp.start('jsBrowserify');
   }
+  gulp.start('bower');
 });
 
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext('js').files)
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('bowerCSS', function () {
+  return gulp.src(lib.ext('css').files)
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+  gulp.watch(['js/*.js'], ['jsBuild']);
+});
+
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
+  browserSync.reload();
 });
